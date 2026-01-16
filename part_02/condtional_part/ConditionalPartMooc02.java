78daf25c7dedf1620226e475cf1a34e76f915e50
franksants <franklinjuansantos@gmail.com>
2026-01-15 23:00:51 -0300

    Reorganizing the Directories and Folders

--- part_02/condtional_part/ConditionalPartMooc02.java
+++ part_02/condtional_part/ConditionalPartMooc02.java
@@ -2,15 +2,13 @@ import java.util.Scanner;
 public class ConditionalPartMooc02{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        String result = "";
+        String result = "Both are equal.";
         int firstNumber = Integer.valueOf(input.nextLine());
         int secondNumber = Integer.valueOf(input.nextLine());
         if(firstNumber < secondNumber)
             result = "The " + secondNumber + " is heaver than " + firstNumber;
-        else if(firstNumber > secondNumber)
-            result = "The " + firstNumber + " is heaver than " + secondNumber;
         else 
-            result = "Both are equals.";
+            result = "The " + firstNumber + " is heaver than " + secondNumber;
 
         System.out.println(result);
         input.close();
